a8defd3888ae2216b5e02cb7313baf0ffd9ed212
jasonxs45 <jasonxs45@126.com>
2020-01-10 16:16:59 +0800

    报修工程师管理员列表添加筛选

--- src/pages/RepairEngineerManager.vue
+++ src/pages/RepairEngineerManager.vue
@@ -15,13 +15,17 @@
         </router-link>
       </flexbox-item>
     </flexbox> -->
+    <div :hidden='!(building||unit||houseno||name)' class='filters'>
+      当前
+      <span v-if='building'>楼栋:{{building}};</span><span v-if='unit'>单元:{{unit}};</span><span v-if='houseno'>房号:{{houseno}};</span><span v-if='name'>姓名:{{name}}</span>
+    </div>
     <div class="content">
-      <div v-if="(engineer[stateType].orders && engineer[stateType].orders.length < 1)|| !engineer[stateType].orders" class="no-data">
+      <div v-if="!lists[currentIndex].length" class="no-data">
         <img src="static/images/repairnodata.png" alt="" />
         <p class="text">暂无数据</p>
       </div>
       <div
-        v-for="(item, index) in engineer[stateType].orders"
+        v-for="(item, index) in lists[currentIndex]"
         :key="'untreatedorder-'+index"
         class="repair-card"
         @click="toDetail(item.ID)"
@@ -46,7 +50,7 @@
             <flexbox-item class="name">
               施工单位：{{item.CompanyName}}{{item.BuilderName}}
             </flexbox-item>
-            <flexbox-item class="tel">
+            <flexbox-item v-if='item.Tel' class="tel">
               <a :href="`tel:${item.Tel}`" @click.stop>{{item.Tel}}</a>
             </flexbox-item>
           </flexbox>
@@ -63,11 +67,13 @@
         </template>
       </div>
       <Getmore
-        v-if="engineer[stateType].orders && engineer[stateType].orders.length > 0"
-        :canClick="!engineer[stateType].lastPage"
-        @click="list"
+        v-if="lists[currentIndex].length"
+        :canClick="!finishes[currentIndex]"
+        @click="more"
       />
     </div>
+    <button :hidden='showFilter' class='filter-btn' @click="openFilter">打开<br />筛选</button>
+    <filter-box :filterShow="showFilter" :initBuilding='building' :initUnit='unit' :initHouseno='houseno' :initName='name' @hide="hideFilter" @confirm="confirm"></filter-box>
   </div>
 </template>
 <script>
@@ -77,19 +83,17 @@ import {
   FlexboxItem,
   Split,
   Star,
-  Getmore
+  Getmore,
+  FilterBox
 } from 'components'
 import {
   formatDate
 } from 'common/utils/date'
+import api from 'common/api'
 let navs = [
   {
     path: 'untreated',
     text: '待审核'
-  },
-  {
-    path: 'timeout',
-    text: '已超时'
   }
 ]
 export default {
@@ -100,13 +104,25 @@ export default {
     FlexboxItem,
     Split,
     Star,
-    Getmore
+    Getmore,
+    FilterBox
   },
   data () {
     return {
       navs,
       stateType: '',
-      role: 'engineermanager'
+      role: 'engineermanager',
+      myrole: '地产客服',
+      showFilter: false,
+      currentIndex: 0,
+      lists: [],
+      pageIndexes: [],
+      finishes: [],
+      searchkey: '',
+      building: '',
+      unit: '',
+      houseno: '',
+      name: ''
     }
   },
   computed: {
@@ -126,13 +142,128 @@ export default {
     }
   },
   created () {
+    if (sessionStorage.building) {
+      this.building = sessionStorage.building
+    }
+    if (sessionStorage.unit) {
+      this.unit = sessionStorage.unit
+    }
+    if (sessionStorage.houseno) {
+      this.houseno = sessionStorage.houseno
+    }
+    if (sessionStorage.name) {
+      this.name = sessionStorage.name
+    }
     this.stateType = this.$route.params.state
     this.stateChangeHandler()
-  },
-  destroyed () {
-    this.reset()
+    this.totalQuery()
   },
   methods: {
+    openFilter () {
+      this.showFilter = true
+    },
+    hideFilter (val) {
+      this.showFilter = val
+    },
+    confirm (val) {
+      const { building, unit, houseno, name } = val
+      this.building = building
+      this.unit = unit
+      this.houseno = houseno
+      this.name = name
+      this.totalQuery()
+      sessionStorage.building = this.building
+      sessionStorage.unit = this.unit
+      sessionStorage.houseno = this.houseno
+      sessionStorage.name = this.name
+    },
+    totalQuery () {
+      const { myrole: role, searchkey, building, unit, houseno, name } = this
+      let stack = []
+      this.lists = []
+      this.finishes = []
+      this.pageIndexes = []
+      for (let i = 0, len = navs.length; i < len; i++) {
+        this.lists.push([])
+        this.finishes.push(true)
+        this.pageIndexes.push(1)
+        stack.push(Promise.resolve(api.repair.list1({
+          role,
+          state: navs[i].value,
+          searchkey,
+          pageindex: 1,
+          building,
+          unit,
+          houseno,
+          name
+        })))
+      }
+      const index = window.$loading('加载中')
+      Promise.all(stack)
+        .then(res => {
+          window.$close(index)
+          res = res.map(item => item.data)
+          this.lists = res.map(item => {
+            let arr
+            if (item.IsSuccess) {
+              arr = item.Data.repairList
+            } else {
+              arr = []
+            }
+            return arr
+          })
+          this.finishes = res.map(item => {
+            let res
+            if (item.IsSuccess) {
+              res = item.Data.lastpage
+            } else {
+              res = true
+            }
+            return res
+          })
+        })
+        .catch(err => {
+          window.$close(index)
+          console.log(err)
+        })
+    },
+    more () {
+      const { searchkey, myrole: role, currentIndex, pageIndexes, finishes, building, unit, houseno, name } = this
+      const state = navs[currentIndex].value
+      const pageindex = pageIndexes[currentIndex] + 1
+      const finished = finishes[currentIndex]
+      console.log(currentIndex)
+      if (!finished) {
+        const index = window.$loading('加载中')
+        api.repair.list1({
+            role,
+            state,
+            searchkey,
+            pageindex,
+            building,
+            unit,
+            houseno,
+            name
+          })
+          .then(res => {
+            window.$close(index)
+            if (res.data.IsSuccess) {
+              this.lists[currentIndex] = this.lists[currentIndex].concat(res.data.Data.repairList)
+              this.finishes[currentIndex] = res.data.Data.lastpage
+              this.pageIndexes[currentIndex] = pageindex
+              this.lists = this.lists.slice()
+              this.finishes = this.finishes.slice()
+              this.pageIndexes = this.pageIndexes.slice()
+            } else {
+              window.$alert(res.data.Message)
+            }
+          })
+          .catch(err => {
+            window.$close(index)
+            console.log(err)
+          })
+      }
+    },
     reset () {
       this.$store.dispatch('repair/destroyed', this.role)
     },
@@ -149,10 +280,7 @@ export default {
         })
         return
       }
-      // this.list(this.stateType)
-      if (this.engineer[this.stateType].page < 1) {
-        this.list(this.stateType)
-      }
+      this.currentIndex = navs.findIndex(item => item.path === this.stateType) === -1 ? 0 : navs.findIndex(item => item.path === this.stateType)
     },
     toDetail (id) {
       this.$router.push({
@@ -168,6 +296,15 @@ export default {
 <style lang="scss" scoped>
 @import "~common/scss/variables.scss";
 @import "~common/scss/mixins.scss";
+.filters{
+    position: fixed;
+    width: 100%;
+    top:0;
+    left: 0;
+    background: rgba(255,255,255,.8);
+    padding: p2r(20) p2r(30);
+    color: $primary-color;
+  }
  .repair-engineer-manager{
    width: 100vw;
    height: 100vh;
@@ -294,5 +431,22 @@ export default {
       }
      }
    }
+   .filter-btn{
+      width: p2r(90);
+      height: p2r(90);
+      position: fixed;
+      right: 10px;
+      bottom: p2r(100);
+      z-index: 99;
+      background: $primary-color;
+      border:none;
+      outline:none;
+      color: #fff;
+      border-radius: 50%;
+      line-height: 1.1;
+      box-shadow: 0 0 20px 0 rgba(0,0,0,.3);
+      font-size: 12px;
+      font-weight: 500;
+    }
  }
 </style>
